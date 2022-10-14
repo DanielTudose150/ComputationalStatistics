ec0ac9369148fc9c4fa8168fefeac2367a991af7
DanielTudose150 <danieltudose150@yahoo.com>
2022-10-14 09:31:32 +0300

    [ADD] 2nd Exercise

--- Lab/2/tema2.R
+++ Lab/2/tema2.R
@@ -1,3 +1,13 @@
 ex1 = function(a, b) {
   plot(log2,from=a,to=b,main=paste0("f(x) = log2(x), x in [",a,", ",b,"]"), col="red", lwd=2);
 }
+
+ex2 = function() {
+  x = 1:20;
+  for(p in seq(0.1,0.9,0.1))
+  {
+    plot(x,dbinom(x,20,p), ylab="y", type="o");
+    title(main=paste0("B(x, 20, ",p,")"));
+    png(paste0("binom",p,".png"),width=600, height=600);
+  }
+}
