e0ab84069c9bc61439406a489537f6ff4fd38817
DanielTudose150 <danieltudose150@yahoo.com>
2022-11-04 00:14:20 +0200

    [ADD] 4th and 5th exercises

--- Lab/3/tema3.R
+++ Lab/3/tema3.R
@@ -20,6 +20,9 @@ ex1 = function() {
   df = data.frame(x,y);
   mat = cov(df);
   print(mat);
+  
+  # deoarece coeficientul de corelatie de negativ, aproape de -1, putem spune ca exista intr-o anumita masura 
+  # o relatie inversa liniar proportionala intre cele doua variabile
 }
 
 ex2 = function() {
@@ -38,6 +41,9 @@ ex2 = function() {
     return(y);
   }
   
+  abline(rsc[1], rsc[2],col="red");
+  legend("topleft", "Y estimator",lty=1,col="red");
+  
   print(paste0("Y = ",rsc[1]," + ", rsc[2], " * X"));
   
   print(paste0("Estimated value for IQ=", 115," = ",estimator(rsc[1],rsc[2],115)));
@@ -48,7 +54,7 @@ ex2 = function() {
 }
 
 ex3 = function(m, a, b, xmin, xmax, sigma) {
-  eps = rnorm(m, sd=sqrt(sigma));
+  eps = rnorm(m, sd=sigma);
   x = runif(m, xmin, xmax);
   y = c();
   
@@ -57,9 +63,74 @@ ex3 = function(m, a, b, xmin, xmax, sigma) {
     y = c(y,ytmp);
   }
   
-  return(y);
+  mat = matrix(cbind(x,y),c(length(x),2));
+  return(mat);
 }
 
-ex4 = function(obs) {
+ex4 = function(m, x, y) {
+  xmean = mean(x);
+  ymean = mean(y);
   
+  xcor = x - xmean;
+  ycor = y - ymean;
+  
+  xy = sum(xcor * ycor);
+  x2 = sum(xcor^2);
+  
+  b = xy / x2;
+  a = ymean - b * xmean;
+  
+  print(paste("Intercept a =",a));
+  print(paste("Slope b =",b));
+  
+  
+  yestimat = x * b + a;
+  
+  sse = sum((y-yestimat)^2)
+  s2 = sse / x2
+  
+  t = qt(0.975,df=x2);
+  
+  left = b - t * (sqrt(s2)/sqrt(x2));
+  right = b + t * (sqrt(s2)/sqrt(x2));
+  
+  print(paste0(left," <= Beta <= ", right));
+  
+  res = cbind(a,b);
+  return(res);
 }
+# ex4(7, seq(100,700,100), c(40,50, 50, 70, 65, 65,80))
+
+ex5 = function() {
+  a = 10;
+  b = 0.8;
+  ms = c(100, 10, 10000, 10, 10000, 10);
+  xmins = c(-200,-5,-5,5,5,5);
+  xmaxs = c(200,5,5,5.2,5.2,5.2);
+  sigmas = c(1.5,1,1,1,1,0.01);
+  
+  colours = c("blue","red");
+  lnames = c("Y = a + bX", "Y^ = a^ + b^ * X");
+  
+  for(i in seq(1,6,1)){
+    mat = ex3(ms[i],a,b,xmins[i],xmaxs[i],sigmas[1]);
+    x = mat[,1];
+    y = mat[,2];
+    ols = ex4(ms[i],x,y);
+    beta0 = ols[1];
+    beta1 = ols[2];
+    
+    pdfTitle = paste0("ex5_",i,".pdf");
+    pdf(pdfTitle,width = 7, height = 7);
+    
+    plot(x,y);
+    
+    
+    abline(a,b,col="blue");
+    abline(beta0,beta1,col="red",lty=2);
+    
+    legend("topleft",legend=lnames,col=colours,lty=c(1,2));
+    
+  }
+}
+
